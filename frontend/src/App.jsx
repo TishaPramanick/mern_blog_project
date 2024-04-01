161ec6f8e835d577ea23e20f8f583f2d97ea449b
TishaP <tishapramanick4890@gmail.com>
2024-04-01 10:56:30 +0530

    create a pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
+import {Routes , BrowserRouter , Route} from "react-router-dom"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Projects from "./pages/Projects";
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h3 className='text-yellow-200'>Heelo</h3>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/sign-in" element={<SignUp/>}></Route>
+        <Route path="/sign-up" element={<SignUp/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/projects" element={<Projects/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
